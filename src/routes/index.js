5b006db2a754faf27d3d61083436020688e259dd
Rian Afrianto <rianafrianto17@gmail.com>
2022-07-04 14:01:06 +0700

    week2

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,5 +1,6 @@
 const productRoutes = require("./productRoutes");
 const authRoutes = require("./authRoutes");
 const adminRoutes = require("./adminRoutes");
+const profileRoutes = require("./profileRoutes");
 
-module.exports = { productRoutes, authRoutes, adminRoutes };
+module.exports = { productRoutes, authRoutes, adminRoutes, profileRoutes };
