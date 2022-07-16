6928c06749c66d17436f423af9739bed476c753e
Pramodana Haekal Ruhyadi <haepram@gmail.com>
2022-07-16 23:13:31 +0700

    code generator fix

--- src/routes/adminRoutes.js
+++ src/routes/adminRoutes.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const { dbCon } = require("../connection");
 const {
   loginAdminController,
   newProduct,
@@ -7,6 +8,7 @@ const {
   validPrescriptionController,
   getProductsController,
 } = require("../controllers");
+const { dateGenerator, codeGenerator } = require("../lib/codeGenerator");
 const Router = express.Router();
 
 Router.post("/adminlogin", loginAdminController);
@@ -15,5 +17,30 @@ Router.get("/filter-products", filterProductsController);
 Router.get("/orders/:status", getOrdersController);
 Router.post("/order/valid-prescription", validPrescriptionController);
 Router.get("/products", getProductsController);
+Router.post("/order", async (req, res) => {
+  // let { id } = req.query;
+  // let { id: user_id } = req.user;
+  let conn, sql;
+  // let { prescription_photo, user_id } = req.body;
+  try {
+    conn = dbCon.promise();
+    let date = dateGenerator();
+    let prescription_photo = "/prescriptions/resep.jpg";
+    let user_id = 65;
+    let insertData = {
+      user_id,
+      prescription_photo,
+      status: 1,
+      transaction_code: codeGenerator("RESEP", date, user_id),
+    };
+    sql = `INSERT INTO orders SET ?`;
+    await conn.query(sql, insertData);
+
+    return res.status(200).send("succeded");
+  } catch (error) {
+    console.log(error);
+    return res.status(500).send("failed");
+  }
+});
 
 module.exports = Router;
